a3a878648bed7e446833db98b9f7918ebf800af6
pong1809 <pongphong1809@gmail.com>
2024-07-17 12:55:28 +0700

    Chapter_7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
 
   validates :name, presence: true,
-  length: {in: 1..50, too_long: I18n.t("users.warnings.max_length_50"),
-           too_short: I18n.t("users.warnings.min_length_1")}
+                   length: { in: 1..50, too_long: I18n.t('users.warnings.max_length_50'),
+                             too_short: I18n.t('users.warnings.min_length_1') }
 
-  validates :email, presence: true, length: {maximum: Settings.max_255},
-    format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+  validates :email, presence: true, length: { maximum: Settings.max_255 },
+                    format: { with: VALID_EMAIL_REGEX }, uniqueness: true
 
   has_secure_password
 
@@ -14,16 +14,14 @@ class User < ApplicationRecord
   around_save :callback_around_save
   after_update :run_callback_after_update
 
-
-
   def callback_around_save
-    Rails.logger.debug "in around save"
+    Rails.logger.debug 'in around save'
     yield
-    Rails.logger.debug "out around save"
+    Rails.logger.debug 'out around save'
   end
-  
+
   def run_callback_after_update
-    Rails.logger.debug "Callback after update is called!"
+    Rails.logger.debug 'Callback after update is called!'
   end
 
   def downcase_email
