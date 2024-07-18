15722a71b7d00bc60b9eff1fe3ab85cba588fbc9
pong1809 <pongphong1809@gmail.com>
2024-07-18 21:52:35 +0700

    Chapter_9

--- db/schema.rb
+++ db/schema.rb
@@ -17,5 +17,7 @@ ActiveRecord::Schema[7.1].define(version: 20_240_708_093_519) do
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.string 'password_digest'
+    t.string 'remember_digest'
+    t.index ['email'], name: 'index_users_on_email', unique: true
   end
 end
